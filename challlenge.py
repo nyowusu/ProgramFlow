573d6a866670740668f581f4772fb5c110ddc599
nyowusu <yaw.owusu@hotmail.com>
2019-05-02 20:07:20 +0100

    Variable names corrected

--- challlenge.py
+++ challlenge.py
@@ -1,36 +1,36 @@
 # take input from user
 
 segments = []
-lenghtOfSegments = []
+lengthOfSegments = []
 segLengthPair = []
 
 checkAnotherIp = True
 
 while checkAnotherIp:
-    ipAddres = input("\nplease enter the IP Address: ").strip()
-    if ipAddres == '':
+    ipAddress = input("\nplease enter the IP Address: ").strip()
+    if ipAddress == '':
         print("Please enter an IP Address\n")
     else:
-        ipSegemnts = ipAddres.split('.')
-        print("There are {0} segments in the address {1}".format(len(ipSegemnts), ipAddres))
+        ipSegments = ipAddress.split('.')
+        print("There are {0} segments in the address {1}".format(len(ipSegments), ipAddress))
 
-        for n in range(0, len(ipSegemnts)):
-            if (n == 0) and (ipSegemnts[n] == ''):
+        for n in range(0, len(ipSegments)):
+            if (n == 0) and (ipSegments[n] == ''):
                 continue
-            segments.append(ipSegemnts[n])
-            lenghtOfSegments.append(len(ipSegemnts[n]))
+            segments.append(ipSegments[n])
+            lengthOfSegments.append(len(ipSegments[n]))
         else:
-            segLengthPair = zip(segments, lenghtOfSegments)
+            segLengthPair = zip(segments, lengthOfSegments)
 
         print("Segment\t\t\tLength of segment")
-        for sgmnt, lngth in segLengthPair:
-            print(sgmnt + "\t\t\t\t" + str(lngth))
+        for segment, length in segLengthPair:
+            print(segment + "\t\t\t\t" + str(length))
 
     goAgain = input("Do you want to check another IP Address? (y/n): ")
     if goAgain == 'n' or goAgain == 'N':
         checkAnotherIp = False
     segments = []
-    lenghtOfSegments = []
+    lengthOfSegments = []
     segLengthPair = []
 
 print('\n')
