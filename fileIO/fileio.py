396751186d8ed28c0d64be4c7b43eeaa23e3fcb3
nyowusu <yaw.owusu@hotmail.com>
2019-05-05 00:13:34 +0100

    Copy of sample.txt created writing.py created

--- fileIO/fileio.py
+++ fileIO/fileio.py
@@ -9,11 +9,47 @@
 #     print(line)
 
 # the pythonic way
-jabber = list()
-with open("sample.txt", 'r') as f:
-    for line in f:
-        jabber.append(line)
+# jabber = list()
+# with open("sample.txt", 'r') as f:
+#     for line in f:
+#         jabber.append(line)
+#
+# print(jabber)
+# for line in jabber:
+#     if 'JAB' in line.upper():
+#         print(line, end='')
 
-print(jabber)
-for line in jabber:
-    print(line)
+# with open("sample.txt", "r") as jabber:
+#     line = jabber.readline()
+#     while line:
+#         print(line, end='')
+#         line = jabber.readline()
+
+with open("sample.txt", "r") as jabber:
+    lines = jabber.readlines()
+    # print(lines, end="")
+
+set_unique_words = set()
+for line in lines:
+    print(line, end='')
+    words = line.split(' ')
+    # print(words)
+    for word in words:
+        if word.strip('\n') != '':
+            set_unique_words.add(word.strip('\n')) #  only the unique words in the list
+        # try:
+        #     set_unique_words.add(word.strip())
+        # except Exception:
+        #     print("Error adding words to set")
+print(set(sorted(set_unique_words)))
+
+
+# writing to a new file called sample_copy
+# with open("sample_copy.txt", "w") as jabberw:
+#     jabberw.writelines(lines) #  this is one way to write to files
+#     print("file written")
+
+## the print function can be used to write to a file
+with open("sample_copy.txt", "w") as jabberw:
+    for line in lines:
+        print(line, file=jabberw, end='')
---
+++ fileIO/writing.py
@@ -0,0 +1,1 @@
+cities = ["Adelaide", "Alice Springs", ]
